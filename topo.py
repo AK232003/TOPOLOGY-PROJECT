395881a5b9f0732a1411112a02b3deb936c2eb8e
AK232003 <76689824+AK232003@users.noreply.github.com>
2022-04-17 22:56:08 +0530

    Update topo.py

--- topo.py
+++ topo.py
@@ -18,41 +18,29 @@ for i in range(0, num_ver):
 
 print("Enter the edges: ")
 for i in range(0, num_edg):
-    # temp=[]
-    # v1 = int(input())
-    # v2 = int(input())
-
+    
     edge_1 = list(map(int, input("Enter comma separated edge vertices: ").split(",")))
     edges.append(edge_1)
-    # temp.append(v1)
-    # temp.append(v2)
-    # edges.append(temp)
 
 print(edges)
 
 img_space = []
-#count = 0
-#print(img_space)
+
 for i in range(0, num_ver):
     temp1=[]
     for j in range(0, num_edg):
-        #print(edges[i][0], edges[i][1], i, j)
         if(edges[j][0] == (i+1)):
-            #print("Hello")
             temp1.append(-1)
-            #count+=1
-            #print(temp1)
+
         elif(edges[j][1] == i+1):
             temp1.append(1)
-            #print(temp1)
+
         else:
             temp1.append(0)
-            #print(temp1)
-        #print(img_space)
+
     img_space.append(temp1)
 
 print()
-# print(img_space)
 for i in range(num_ver):
     for j in range(num_edg):
         if(img_space[i][j] >= 0):
@@ -62,9 +50,7 @@ for i in range(num_ver):
     print()
 
 rank = sym.Matrix(img_space).rank()
-#A = RREF
-#print(RREF)
-#print(A[0])
+
 betti_0 = num_ver - rank
 
 print("==========")
